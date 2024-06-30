6ce57ecf3a2ff2f8ad10c0bde784d17acbe97d5c
Mohammed Fasil V <fasilmveloor@gmail.com>
2024-06-30 20:29:29 +0530

    create crossword puzzle with english with codelab tutorial

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'widgets/crossword_puzzle_app.dart'; 
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ProviderScope(
+      child: MaterialApp(
+        title: 'Crossword Puzzle',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+          colorSchemeSeed: Colors.blueGrey,
+          brightness: Brightness.light,
+        ),
+        home: CrosswordPuzzleApp(),
+      ),
+    );
+  }
+}
